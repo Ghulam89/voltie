623a4907ae04ca4412fb747eb673c72639f18cc3
Ghulam89 <gm414157@gmail.com>
2024-02-09 12:40:30 -0800

    update

--- src/components/AppPage/AppPage.tsx
+++ src/components/AppPage/AppPage.tsx
@@ -8,25 +8,25 @@ import { manHoldingPhone  ,staytuned,blackbg} from '@/assets/images'
 const AppPage = () => {
     return (
         <section className='relative h-full w-full overflow-hidden'>  
-        <Image src={blackbg} layout='fill' objectFit='cover' alt='slider-image-first'/>  
+        <Image src={staytuned} layout='fill' objectFit='cover' alt='slider-image-first'/>  
                 <div className='h-[100vh] w-full'>
            
                 <div className='absolute z-10 text-white w-full h-full '>
              <div className='flex w-full content-center justify-cente h-full'>
                 <div className='w-[70%] px-[5%] text-white gap-y-4 sm:gap-y-8 flex flex-col justify-center my-auto '>
                         {/* <Heading name="Plan B" /> */}
-                        <SubHeading name="Partner Managed Service" />
-                        <Description  name='Our Partner Plan gives Owners the opportunity to receive a larger percentage of the of the estimated monthly revenue while also benefiting from VoltiE’s Management Services.This Plan is great for Fleet Operators and mid-sized Property Owners. A smaller percentage of the revenue pays for Platform Management'/>
-                        <div className='flex justify-center md:justify-start items-center gap-2 md:gap-x-4 flex-wrap lg:flex-nowrap'>
+                        {/* <SubHeading name="Partner Managed Service" /> */}
+                        {/* <Description  name='Our Partner Plan gives Owners the opportunity to receive a larger percentage of the of the estimated monthly revenue while also benefiting from VoltiE’s Management Services.This Plan is great for Fleet Operators and mid-sized Property Owners. A smaller percentage of the revenue pays for Platform Management'/> */}
+                        {/* <div className='flex justify-center md:justify-start items-center gap-2 md:gap-x-4 flex-wrap lg:flex-nowrap'>
                             <Button name="Contact us" bgColor='white' />
-                        </div>
+                        </div> */}
                 </div>
                
                 <div className='w-[40%] px-[5%] text-white gap-y-4 sm:gap-y-8 hidden md:flex flex-col justify-center my-auto '>
-                <Image src={staytuned} alt='details-section-image-first' 
+                {/* <Image src={staytuned} alt='details-section-image-first' 
                         objectFit='contain'
                         style={{ height: "35%"}}
-                    />
+                    /> */}
                </div>
              </div>
                 
